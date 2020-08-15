40a11c788f49a395c41f852c7c8ae3dc0eb8250a
azmeena1311 <azminavanzara@gmail.com>
2020-08-15 21:44:40 +0530

    Login

--- E-commerce/E-shop/src/app/component/login/login.component.ts
+++ E-commerce/E-shop/src/app/component/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form :HTMLFormElement
+  error : boolean = false;
+  success :boolean = false;  
+  errorMessage :String;
+  successMessage :String;
+   
 
-  constructor() { }
+
+  constructor(private userservice : UserService, private router: Router) { }
 
   ngOnInit(): void {
   }
+
+  login(event :Event){
+    event.preventDefault();
+    console.log(event.target);
+    this.form = <HTMLFormElement>event.target
+    this.readFormValues();
+  }
+
+readFormValues(){
+
+  let email = (<HTMLFormElement>this.form.elements.namedItem('email')).value
+  let password = (<HTMLFormElement>this.form.elements.namedItem('password')).value
+
+  let credentials = {
+    email,password
+  }
+console.log(credentials);
+
+this.userservice.login(credentials).subscribe({
+  next :(result)=>{
+    console.log(result);
+    this.success = true;
+    this.error = false;
+    this.successMessage = result.message;
+    
+    this.navigateToHomePage()
+   
+  },
+  error : (response :HttpErrorResponse)=>{
+    console.log(response.error);
+    this.error = true;
+          this.success = false;
+          this.errorMessage ="User in the database already"
+          
+  }
+})
+
+}
+  navigateToHomePage() {
+    this.router.navigate(['home'])
+      }
+
 
 }
--- E-commerce/E-shop/src/app/services/user.service.ts
+++ E-commerce/E-shop/src/app/services/user.service.ts
@@ -2,16 +2,41 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { from } from 'rxjs';
 import { User } from '../models/user.model';
-
+import{map} from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  private userSignupUrl ="http://localhost/api/users/signup"
+  private userSignupUrl ="http://localhost/api/users/signup";
+
+  private userloginurl = "http://localhost/api/users/login";
   constructor(private http :HttpClient) { }
 
   signup(user :User){
     return this.http.post(this.userSignupUrl,user)
+    .pipe(
+      map(result =>{
+      return  <{message :string}>result
+      })
+    )
   }
+
+
+  login(credentials :{email :string,password :string}){
+
+  return  this.http.post(this.userloginurl,credentials)
+  .pipe(
+    map(result =>{
+      return <loginResponse>result
+    })
+  )
+
+  }
+}
+
+
+interface loginResponse{
+  token :string,
+  message :string
 }
